0c75a54c2e0cdbea3b3b482c337459a8a13b3d04
Taylor Dettling <taylor@binarynightowl.com>
2020-03-24 14:14:48 -0500

    Improvements and tweaks
    Signed-off-by: Taylor Dettling <taylor@binarynightowl.com>

--- main.py
+++ main.py
@@ -5,41 +5,21 @@ scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/au
 cred_file = 'client-secret.json'  # client secret file, do not make this file public!
 sheet_name = "COVID-19-CasesOverTime"  # what google sheet to use
 
-# crappy code fix this later - maybe find a way to shorten urls if at all possible
 world_url = 'https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/Coronavirus_2019_nCoV_Cases' \
-            '/FeatureServer/1/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope' \
-            '&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter' \
-            '&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption' \
-            '=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection' \
-            '=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false' \
-            '&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields' \
-            '=&groupByFieldsForStatistics=&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C' \
-            '%22onStatisticField%22%3A%22Confirmed%22%2C%22outStatisticFieldName%22%3A%22confirmed%22%7D%2C%7B' \
-            '%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22deaths%22%2C%22outStatisticFieldName' \
-            '%22%3A%22deaths%22%7D%2C%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22recovered' \
-            '%22%2C%22outStatisticFieldName%22%3A%22recovered%22%7D%5D&having=&resultOffset=&resultRecordCount' \
-            '=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat' \
-            '=none&f=pjson&token= '
+            '/FeatureServer/1/query?where=1=1&outStatistics=[{%22statisticType%22:%22sum%22,' \
+            '%22onStatisticField%22:%22Confirmed%22,%22outStatisticFieldName%22:%22confirmed%22},' \
+            '{%22statisticType%22:%22sum%22,%22onStatisticField%22:%22deaths%22,' \
+            '%22outStatisticFieldName%22:%22deaths%22},{%22statisticType%22:%22sum%22,' \
+            '%22onStatisticField%22:%22recovered%22,%22outStatisticFieldName%22:%22recovered%22}]&f=pjson'
 china_us_url = 'https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/ncov_cases/FeatureServer/2/query' \
-               '?where=1%3D1&objectIds=18%2C4&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel' \
-               '=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false' \
-               '&outFields=+Country_Region%2C+confirmed%2C+deaths%2C+recovered%2C+active&returnGeometry=false' \
-               '&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision' \
-               '=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false' \
-               '&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false' \
-               '&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset' \
-               '=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true' \
-               '&quantizationParameters=&sqlFormat=none&f=pjson&token= '
+               '?where=1=1&objectIds=18,4&outFields=%20Country_Region,%20confirmed,%20deaths,%20recovered,' \
+               '%20active&returnGeometry=false&f=pjson'
 state_url = 'https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/ncov_cases/FeatureServer/1/query' \
-            '?where=Country_Region%3D%27US%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR' \
-            '=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic' \
-            '=false&outFields=Province_State%2C++confirmed%2C+deaths%2C+recovered%2C+active&returnGeometry=false' \
-            '&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR' \
-            '=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false' \
-            '&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false' \
-            '&cacheHint=true&orderByFields=confirmed&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset' \
-            '=0&resultRecordCount=200&returnZ=false&returnM=false&returnExceededLimitFeatures=true' \
-            '&quantizationParameters=&sqlFormat=none&f=pjson&token='
+            '?where=Country_Region=%27US%27&outFields=Province_State,confirmed,deaths,recovered,' \
+            '%20active&resultRecordCount=200&f=pjson'
+
+state_data = {
+}
 
 
 # get data for any state based on state name
@@ -48,45 +28,43 @@ def get_state_data(state_to_find):
     for state in document:
         state = state['attributes']
         if state['Province_State'] == state_to_find:
-            return state['Confirmed']
+            state_data[state_to_find] = (state['Confirmed'] - state['Deaths'] - state['Recovered'])
 
 
 # get data for all states in a given list and return it as a list
 def get_all_state_data():
     states = ["New York", "Washington", "New Jersey", "California", "Illinois", "Michigan", "Florida", "Louisiana",
               "Massachusetts", "Texas", "Georgia", "Colorado"]
-    values = []
     for state in states:
-        values.append(get_state_data(state))
-    return values  # there's gotta be a better way to do this but I am sleepy
+        get_state_data(state)
 
 
-# create a seperate sheets instance per workbook, by number
+# create a separate sheets instance per workbook, by number
 world = gsheets.Sheet(cred_file, scope, sheet_name, 1)
 china = gsheets.Sheet(cred_file, scope, sheet_name, 2)
 us = gsheets.Sheet(cred_file, scope, sheet_name, 3)
 
 # scrape the data using a json search query and then read the appropriate information from the json document
-data = json.loads(urllib.request.urlopen(world_url).read().decode())  # crappy code fix this later
+data = json.loads(urllib.request.urlopen(world_url).read().decode())
 total_cases = data['features'][0]['attributes']['confirmed']
 total_deaths = data['features'][0]['attributes']['deaths']
 total_recovered = data['features'][0]['attributes']['recovered']
 
 # scrape more data using a json search query and then read the appropriate information from the json document
-data = json.loads(urllib.request.urlopen(china_us_url).read().decode())  # crappy code fix this later
+data = json.loads(urllib.request.urlopen(china_us_url).read().decode())
 china_cases = data['features'][0]['attributes']['Confirmed']
 china_deaths = data['features'][0]['attributes']['Deaths']
 china_recovered = data['features'][0]['attributes']['Recovered']
 us_cases = data['features'][1]['attributes']['Confirmed']
 us_deaths = data['features'][1]['attributes']['Deaths']
 us_recovered = data['features'][1]['attributes']['Recovered']
+get_all_state_data()
 
-# # write the scraped data to the appropriate Google Sheets
+# write the scraped data to the appropriate Google Sheets
 world.write_data(total_cases, total_deaths, total_recovered)
 china.write_data(china_cases, china_deaths, china_recovered)
 us.write_data(us_cases, us_deaths, us_recovered)
-us.write_state_data(get_all_state_data())  # crappy code fix this later
+us.write_state_data(state_data)
 
 # cleanup / exit
-print('Success!')
 quit(0)
--- scripts/sheets.py
+++ scripts/sheets.py
@@ -1,6 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import datetime
+import datetime as dt
 
 
 class Sheet:
@@ -9,18 +9,18 @@ class Sheet:
         # create gsheets credentials
         self.credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials, gapi_scope)
         self.sheet_name = sheet_name
-        self.workbok_num = workbook_num - 1
+        self.workbook_num = workbook_num - 1
         # create gsheets client
         self.client = gspread.authorize(self.credentials)
 
-        document = self.client.open("COVID-19-CasesOverTime")
-        self.sheet = document.get_worksheet(self.workbok_num)
-
+        document = self.client.open(self.sheet_name)
+        self.sheet = document.get_worksheet(self.workbook_num)
+        # Scan for the total number of rows and set the current row to 1 longer
+        self.current_line = self.sheet.row_count
+        self.row_to_write = self.current_line + 1
         self.get_old_data()
 
     def get_old_data(self):  # not currently  used - will be implemented later
-        # Scan for the total number of rows and set the current row to 1 longer
-        self.current_line = self.sheet.row_count
         self.old_cases = self.sheet.cell(self.current_line, 2).value
         self.old_deaths = self.sheet.cell(self.current_line, 3).value
         self.old_recovered = self.sheet.cell(self.current_line, 4).value
@@ -31,13 +31,11 @@ class Sheet:
 
     def write_data(self, cases, deaths, recovered):
         active = (cases - deaths) - recovered
-        self.row_to_write = self.current_line + 1  # crappy code fix this later
-        current_time = datetime.datetime.now()
+        current_time = dt.datetime.now()
         self.sheet.resize(self.row_to_write)
-        # there's gotta be a better way to do this but I am sleepy
         self.sheet.update_cell(self.row_to_write, 1,  # crappy code fix this later
                                "{}-{}-{}".format(current_time.year, current_time.month, current_time.day))
-        self.sheet.update_cell(self.row_to_write, 2, cases)
+        self.sheet.update_cell(self.row_to_write, 2, cases)  # row, column, data to write to cell
         self.sheet.update_cell(self.row_to_write, 3, deaths)
         self.sheet.update_cell(self.row_to_write, 4, recovered)
         self.sheet.update_cell(self.row_to_write, 5, active)
@@ -45,17 +43,16 @@ class Sheet:
         self.sheet.update_cell(self.row_to_write, 8, recovered / cases)
         self.sheet.update_cell(self.row_to_write, 9, active / cases)
 
-    def write_state_data(self, state_data):  # there's gotta be a better way to do this but I am sleepy
-        ny, wa, nj, ca, il, mi, fl, la, ma, tx, ga, co = state_data  # crappy code fix this later
-        self.sheet.update_cell(self.row_to_write, 13, ny)
-        self.sheet.update_cell(self.row_to_write, 14, wa)
-        self.sheet.update_cell(self.row_to_write, 15, nj)
-        self.sheet.update_cell(self.row_to_write, 16, ca)
-        self.sheet.update_cell(self.row_to_write, 17, il)
-        self.sheet.update_cell(self.row_to_write, 18, mi)
-        self.sheet.update_cell(self.row_to_write, 19, fl)
-        self.sheet.update_cell(self.row_to_write, 20, la)
-        self.sheet.update_cell(self.row_to_write, 21, ma)
-        self.sheet.update_cell(self.row_to_write, 22, tx)
-        self.sheet.update_cell(self.row_to_write, 23, ga)
-        self.sheet.update_cell(self.row_to_write, 24, co)
+    def write_state_data(self, state_data):
+        self.sheet.update_cell(self.row_to_write, 13, state_data['New York'])
+        self.sheet.update_cell(self.row_to_write, 14, state_data['Washington'])
+        self.sheet.update_cell(self.row_to_write, 15, state_data['New Jersey'])
+        self.sheet.update_cell(self.row_to_write, 16, state_data['California'])
+        self.sheet.update_cell(self.row_to_write, 17, state_data['Illinois'])
+        self.sheet.update_cell(self.row_to_write, 18, state_data['Michigan'])
+        self.sheet.update_cell(self.row_to_write, 19, state_data['Florida'])
+        self.sheet.update_cell(self.row_to_write, 20, state_data['Louisiana'])
+        self.sheet.update_cell(self.row_to_write, 21, state_data['Massachusetts'])
+        self.sheet.update_cell(self.row_to_write, 22, state_data['Texas'])
+        self.sheet.update_cell(self.row_to_write, 23, state_data['Georgia'])
+        self.sheet.update_cell(self.row_to_write, 24, state_data['Colorado'])
