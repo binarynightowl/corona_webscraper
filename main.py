de9cb0444c7d6f22dbb1df481ed90db9f6cbe5c5
e85828 <gerald.dettling@wnco.com>
2020-03-30 20:06:39 -0500

    Added all US status and DC

--- main.py
+++ main.py
@@ -17,8 +17,16 @@ def get_state_data(state_to_find):
 
 # get data for all states in a given list and store it to a dictionary
 def get_all_state_data():
-    states = ["NewYork", "Washington", "NewJersey", "California", "Illinois", "Michigan", "Florida", "Louisiana",
-              "Massachusetts", "Texas", "Georgia", "Colorado"]
+    states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado",
+              "Connecticut","Delaware","District of Columbia","Florida",
+              "Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas",
+              "Kentucky","Louisiana","Maine","Montana","Nebraska","Nevada",
+              "New Hampshire","New Jersey","New Mexico","New York",
+              "North Carolina","North Dakota","Ohio","Oklahoma","Oregon",
+              "Maryland","Massachusetts","Michigan","Minnesota","Mississippi",
+              "Missouri","Pennsylvania","Rhode Island","South Carolina",
+              "South Dakota","Tennessee","Texas","Utah","Vermont","Virginia",
+              "Washington","West Virginia","Wisconsin","Wyoming"]
     for state in states:
         get_state_data(state)
 
