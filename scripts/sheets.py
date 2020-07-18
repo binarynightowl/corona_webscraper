1a1343db44d9476e5f6cdd6c1a666d58e8f90104
Taylor Dettling <taylor@binarynightowl.com>
2020-07-17 22:27:14 -0500

    Added column and dynamically write data to all columns

--- scripts/sheets.py
+++ scripts/sheets.py
@@ -22,32 +22,31 @@ class Sheet:
         self.row_to_write = self.current_line + 1
         self.previous_row = self.current_line
 
-        self.get_old_data()
         self.column_headers = self.sheet.row_values(1)
-        print(self.column_headers)
 
-    def get_old_data(self):  # not currently  used - will be implemented later
-        self.old_cases = self.sheet.cell(self.current_line, 2).value
-        self.old_deaths = self.sheet.cell(self.current_line, 3).value
-        self.old_recovered = self.sheet.cell(self.current_line, 4).value
-        self.old_active = self.sheet.cell(self.current_line, 5).value
-        self.old_deaths_per = self.sheet.cell(self.current_line, 7).value
-        self.old_recovered_per = self.sheet.cell(self.current_line, 8).value
-        self.old_active_per = self.sheet.cell(self.current_line, 9).value
-
-    def write_data(self, cases, deaths, recovered):
-        active = (cases - deaths) - recovered
-        current_time = strftime("%Y-%m-%d %H:%M:%S")
-        self.sheet.resize(self.row_to_write)
-        self.sheet.update_cell(self.row_to_write, 1, current_time)
-        self.sheet.update_cell(self.row_to_write, 2, cases)  # row, column, data to write to cell
-        self.sheet.update_cell(self.row_to_write, 3, deaths)
-        self.sheet.update_cell(self.row_to_write, 4, recovered)
-        self.sheet.update_cell(self.row_to_write, 5, active)
-        self.sheet.update_cell(self.row_to_write, 7, deaths / cases)
-        self.sheet.update_cell(self.row_to_write, 8, recovered / cases)
-        self.sheet.update_cell(self.row_to_write, 9, active / cases)
+    def getColumnIndex(self, name):
+        return self.column_headers.index(name) + 1
 
     def write_state_data(self, state_data):
         for state in self.state_dict:
             self.sheet.update_cell(self.row_to_write, self.column_headers.index(state) + 1, state_data[state])
+
+    def write_data(self, cases, deaths, recovered):
+        active = (cases - deaths) - recovered
+        current_time = strftime("%Y-%m-%d %H:%M:%S")
+
+        self.sheet.resize(self.row_to_write)
+        headers = {
+            "Date": current_time,
+            "Cases": cases,
+            "Deaths": deaths,
+            "Recovered": recovered,
+            "Active": active,
+            "pDeaths": deaths / cases,
+            "pRecovered": recovered / cases,
+            "pActive": active / cases,
+            "R/D": recovered / deaths
+        }
+
+        for item in headers:
+            self.sheet.update_cell(self.row_to_write, self.getColumnIndex(item), str(headers[item]))
